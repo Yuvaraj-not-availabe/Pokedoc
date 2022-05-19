e0ec63f80585706351c8e5f40b5f6c8893baba9b
yuvaraj-pt4283 <yuvaraj.mk@zohocorp.com>
2022-05-19 12:50:57 +0530

    initial

---
+++ app/build/tmp/kapt3/stubs/debug/com/plcoding/jetpackcomposepokedex/ui/theme/ColorKt.java
@@ -0,0 +1,137 @@
+package com.plcoding.jetpackcomposepokedex.ui.theme;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\n\n\u0000\n\u0002\u0018\u0002\n\u0002\b6\"\u0016\u0010\u0000\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0002\u0010\u0003\"\u0016\u0010\u0005\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0006\u0010\u0003\"\u0016\u0010\u0007\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\b\u0010\u0003\"\u0016\u0010\t\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\n\u0010\u0003\"\u0016\u0010\u000b\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\f\u0010\u0003\"\u0016\u0010\r\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u000e\u0010\u0003\"\u0016\u0010\u000f\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0010\u0010\u0003\"\u0016\u0010\u0011\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0012\u0010\u0003\"\u0016\u0010\u0013\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0014\u0010\u0003\"\u0016\u0010\u0015\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0016\u0010\u0003\"\u0016\u0010\u0017\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u0018\u0010\u0003\"\u0016\u0010\u0019\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u001a\u0010\u0003\"\u0016\u0010\u001b\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u001c\u0010\u0003\"\u0016\u0010\u001d\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\u001e\u0010\u0003\"\u0016\u0010\u001f\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b \u0010\u0003\"\u0016\u0010!\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b\"\u0010\u0003\"\u0016\u0010#\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b$\u0010\u0003\"\u0016\u0010%\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b&\u0010\u0003\"\u0016\u0010\'\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b(\u0010\u0003\"\u0016\u0010)\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b*\u0010\u0003\"\u0016\u0010+\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b,\u0010\u0003\"\u0016\u0010-\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b.\u0010\u0003\"\u0016\u0010/\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b0\u0010\u0003\"\u0016\u00101\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b2\u0010\u0003\"\u0016\u00103\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b4\u0010\u0003\"\u0016\u00105\u001a\u00020\u0001\u00f8\u0001\u0000\u00a2\u0006\n\n\u0002\u0010\u0004\u001a\u0004\b6\u0010\u0003\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u00067"}, d2 = {"AtkColor", "Landroidx/compose/ui/graphics/Color;", "getAtkColor", "()J", "J", "DefColor", "getDefColor", "HPColor", "getHPColor", "LightBlue", "getLightBlue", "SpAtkColor", "getSpAtkColor", "SpDefColor", "getSpDefColor", "SpdColor", "getSpdColor", "TypeBug", "getTypeBug", "TypeDark", "getTypeDark", "TypeDragon", "getTypeDragon", "TypeElectric", "getTypeElectric", "TypeFairy", "getTypeFairy", "TypeFighting", "getTypeFighting", "TypeFire", "getTypeFire", "TypeFlying", "getTypeFlying", "TypeGhost", "getTypeGhost", "TypeGrass", "getTypeGrass", "TypeGround", "getTypeGround", "TypeIce", "getTypeIce", "TypeNormal", "getTypeNormal", "TypePoison", "getTypePoison", "TypePsychic", "getTypePsychic", "TypeRock", "getTypeRock", "TypeSteel", "getTypeSteel", "TypeWater", "getTypeWater", "lightGrey", "getLightGrey", "app_debug"})
+public final class ColorKt {
+    private static final long LightBlue = 0L;
+    private static final long lightGrey = 0L;
+    private static final long HPColor = 0L;
+    private static final long AtkColor = 0L;
+    private static final long DefColor = 0L;
+    private static final long SpAtkColor = 0L;
+    private static final long SpDefColor = 0L;
+    private static final long SpdColor = 0L;
+    private static final long TypeNormal = 0L;
+    private static final long TypeFire = 0L;
+    private static final long TypeWater = 0L;
+    private static final long TypeElectric = 0L;
+    private static final long TypeGrass = 0L;
+    private static final long TypeIce = 0L;
+    private static final long TypeFighting = 0L;
+    private static final long TypePoison = 0L;
+    private static final long TypeGround = 0L;
+    private static final long TypeFlying = 0L;
+    private static final long TypePsychic = 0L;
+    private static final long TypeBug = 0L;
+    private static final long TypeRock = 0L;
+    private static final long TypeGhost = 0L;
+    private static final long TypeDragon = 0L;
+    private static final long TypeDark = 0L;
+    private static final long TypeSteel = 0L;
+    private static final long TypeFairy = 0L;
+    
+    public static final long getLightBlue() {
+        return 0L;
+    }
+    
+    public static final long getLightGrey() {
+        return 0L;
+    }
+    
+    public static final long getHPColor() {
+        return 0L;
+    }
+    
+    public static final long getAtkColor() {
+        return 0L;
+    }
+    
+    public static final long getDefColor() {
+        return 0L;
+    }
+    
+    public static final long getSpAtkColor() {
+        return 0L;
+    }
+    
+    public static final long getSpDefColor() {
+        return 0L;
+    }
+    
+    public static final long getSpdColor() {
+        return 0L;
+    }
+    
+    public static final long getTypeNormal() {
+        return 0L;
+    }
+    
+    public static final long getTypeFire() {
+        return 0L;
+    }
+    
+    public static final long getTypeWater() {
+        return 0L;
+    }
+    
+    public static final long getTypeElectric() {
+        return 0L;
+    }
+    
+    public static final long getTypeGrass() {
+        return 0L;
+    }
+    
+    public static final long getTypeIce() {
+        return 0L;
+    }
+    
+    public static final long getTypeFighting() {
+        return 0L;
+    }
+    
+    public static final long getTypePoison() {
+        return 0L;
+    }
+    
+    public static final long getTypeGround() {
+        return 0L;
+    }
+    
+    public static final long getTypeFlying() {
+        return 0L;
+    }
+    
+    public static final long getTypePsychic() {
+        return 0L;
+    }
+    
+    public static final long getTypeBug() {
+        return 0L;
+    }
+    
+    public static final long getTypeRock() {
+        return 0L;
+    }
+    
+    public static final long getTypeGhost() {
+        return 0L;
+    }
+    
+    public static final long getTypeDragon() {
+        return 0L;
+    }
+    
+    public static final long getTypeDark() {
+        return 0L;
+    }
+    
+    public static final long getTypeSteel() {
+        return 0L;
+    }
+    
+    public static final long getTypeFairy() {
+        return 0L;
+    }
+}
